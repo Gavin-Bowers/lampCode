e8ce636f4d4a10aa7b659bc480f01438ca2998d7
Gavin Bowers <45107946+Ghavyn@users.noreply.github.com>
2022-05-14 13:33:49 -0700

    Add files via upload
    Added port as a parameter as well as audio sample size and smoothing. Removed some unused code

--- LampAudioVisualizer/ls.py
+++ LampAudioVisualizer/ls.py
@@ -1,4 +1,5 @@
 import argparse
+from tokenize import String
 from src.stream_analyzer import Stream_Analyzer
 import time
 import serial
@@ -7,15 +8,14 @@ def parse_args():
     parser = argparse.ArgumentParser()
     parser.add_argument('--device', type=int, default=6, dest='device',
                         help='pyaudio (portaudio) device index')
-    parser.add_argument('--height', type=int, default=450, dest='height',
-                        help='height, in pixels, of the visualizer window')
     parser.add_argument('--n_frequency_bins', type=int, default=35, dest='frequency_bins',
                         help='The FFT features are grouped in bins')
-    parser.add_argument('--verbose', action='store_true')
-    parser.add_argument('--window_ratio', default='24/9', dest='window_ratio',
-                        help='float ratio of the visualizer window. e.g. 24/9')
-    parser.add_argument('--sleep_between_frames', dest='sleep_between_frames', action='store_true',
-                        help='when true process sleeps between frames to reduce CPU usage (recommended for low update rates)')
+    parser.add_argument('--port', type=int, default=4, dest='port',
+                        help='The serial port the lamp is connected to'),
+    parser.add_argument('--window_size', type=int, default=20, dest='window_size',
+                        help='The size of audio samples in ms'),
+    parser.add_argument('--smoothing_length', type=int, default=20, dest='smoothing_length',
+                        help='The length of the audio smoothing sample in ms')
     return parser.parse_args()
 
 def convert_window_ratio(window_ratio):
@@ -30,29 +30,28 @@ def convert_window_ratio(window_ratio):
 
 def run_FFT_analyzer():
     args = parse_args()
-    window_ratio = convert_window_ratio(args.window_ratio)
+    window_ratio = convert_window_ratio('24/9')
 
     ear = Stream_Analyzer(
                     device = args.device,        # Pyaudio (portaudio) device index, defaults to first mic input
                     rate   = None,               # Audio samplerate, None uses the default source settings
-                    FFT_window_size_ms  = 60,    # Window size used for the FFT transform
-                    updates_per_second  = 1000,  # How often to read the audio stream for new data
-                    smoothing_length_ms = 50,    # Apply some temporal smoothing to reduce noisy features
+                    FFT_window_size_ms  = args.window_size,    # Window size used for the FFT transform
+                    smoothing_length_ms = args.smoothing_length,    # Apply some temporal smoothing to reduce noisy features
                     n_frequency_bins = args.frequency_bins, # The FFT features are grouped in bins
+                    serial_port = args.port,
+                    updates_per_second  = 4000,  # How often to read the audio stream for new data
                     visualize = 0,               # Visualize the FFT features with PyGame
-                    verbose   = args.verbose,    # Print running statistics (latency, fps, ...)
-                    height    = args.height,     # Height, in pixels, of the visualizer window,
-                    window_ratio = window_ratio  # Float ratio of the visualizer window. e.g. 24/9
+                    verbose   = False,    # Print running statistics (latency, fps, ...)
+                    height    = 450,     # Height, in pixels, of the visualizer window,
+                    window_ratio = '24/9'  # Float ratio of the visualizer window. e.g 24/9
                     )
 
-    fps = 60  #How often to update the FFT features + display
+    fps = 400  #How often to update the FFT features + display
     last_update = time.time()
     while True:
         if (time.time() - last_update) > (1./fps):
             last_update = time.time()
             raw_fftx, raw_fft, binned_fftx, binned_fft = ear.get_audio_features()
-        elif args.sleep_between_frames:
-            time.sleep(((1./fps)-(time.time()-last_update)) * 0.99)
 
 if __name__ == '__main__':
     run_FFT_analyzer()
