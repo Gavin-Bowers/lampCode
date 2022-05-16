fd4c06eae1139c2350973666ad865f4124336db6
Gavin Bowers <45107946+Ghavyn@users.noreply.github.com>
2022-05-15 17:18:49 -0700

    Huge improvements

--- code.py
+++ code.py
@@ -1,7 +1,6 @@
-#Imports
 import supervisor
 import board
-import neopixel #If missing, download adafruit circuitpython 6.X bundle and move neopixel.mpy into the drive for this device. Now bundled with this program
+import neopixel #If missing, copy from the lightshow folder onto this device
 import time
 from digitalio import DigitalInOut, Direction, Pull
 import analogio
@@ -15,26 +14,23 @@ import array
 
 #Setup
 
-#Audio (audio isn't actually supported, it just bugs out on my device without this)
-mic = audiobusio.PDMIn(clock_pin=board.GP3, data_pin=board.GP2,mono=True, sample_rate=16000, bit_depth=16)
+smallLamp = False #Change this to True for easy small lamp setup
+modes = [[None for i in range(2)] for i in range(2)] #Buffer of 2 needed for some reason
 
-#Button Setup
-btn = DigitalInOut(board.GP9) #GP2 for smaller lamps <--
-btn.direction = Direction.INPUT
-btn.pull = Pull.UP
+if smallLamp:
+    N_PIXELS = 34 #Number of LEDs
+    BUFFER = 0 #The number of pixels you don't want to use at the start of the LED strip
+    HALF = math.floor((N_PIXELS-BUFFER)/2.0) #If the (N_PIXELS - BUFFER) is odd, stuff might break
+    pixels = neopixel.NeoPixel(board.GP0, N_PIXELS, brightness=1, auto_write=False)
+    btn = DigitalInOut(board.GP2)
 
-#Led Setup
-pixelCount = 51 #Number of LEDs. 34 for smaller lamps <--
-buffer = 1 #The number of pixels you don't want to use at the start of the strip. Probably should be 0
-half = math.floor((pixelCount-buffer)/2.0) #If the (pixelCount - buffer) is odd, stuff might break
-pixels = neopixel.NeoPixel(board.GP5, pixelCount, brightness=1, auto_write=False) #GP0 for smaller lamps <--
-
-#General Parameters
-pressed = False #Button value
-dcWait = 0 #Double click timer
-animationMode = 0 #Which animation is displaying
-brightness = 1 #LED Brightness
-brIncrement = 0.2 #Amount the brightness changes per button press. 0 to 1. default 0.2
+else:
+    N_PIXELS = 51 #Number of LEDs
+    BUFFER = 1 #The number of pixels you don't want to use at the start of the LED strip
+    HALF = math.floor((N_PIXELS-BUFFER)/2.0) #If the (N_PIXELS - BUFFER) is odd, stuff might break
+    pixels = neopixel.NeoPixel(board.GP5, N_PIXELS, brightness=1, auto_write=False) #GP0 for smaller lamps
+    mic = audiobusio.PDMIn(clock_pin=board.GP3, data_pin=board.GP2,mono=True, sample_rate=16000, bit_depth=16) #My device bugs out without this
+    btn = DigitalInOut(board.GP9)
 
 #Utilities
 
@@ -44,30 +40,40 @@ def wheel(x, blending): #(Turns 0-255 into color from r-g-b. b is for blending, 
 
 def colorCalc(colors, x, blending, r): #r is for range
     d = len(colors)
-    wRed = 0.0 #sum of weighted red values
-    sumRed = 0.0 #sum of red weights
-    wGreen = 0.0
-    sumGreen = 0.0
-    wBlue = 0.0
-    sumBlue = 0.0
+    weightedColors = []
     for i in range(d):
         weight = max(0,(math.cos((2*math.pi*(x-((i*r)/d)))/r)+blending)/(1+blending))
-        wRed += colors[i][0] * weight
-        sumRed += weight
-        wGreen += colors[i][1] * weight
-        sumGreen += weight
-        wBlue += colors[i][2] * weight
-        sumBlue += weight
-    red = math.floor(wRed / sumRed)
-    green = math.floor(wGreen / sumGreen)
-    blue = math.floor(wBlue / sumBlue)
-    color = (red,green,blue)
-    return(color)
+        weightedColors.append((colors[i][0],colors[i][1],colors[i][2],weight))
+    return(colorWeightedAverage(weightedColors))
+
+def colorWeightedAverage(weightedColors): #[red,green,blue,weight]
+    wrs = 0.0 #weighted red sum
+    wgs = 0.0 #weighted green sum
+    wbs = 0.0 #weighted blue sum
+    sw = 0.0 #sum of weights
+    for wc in weightedColors:
+        wrs += wc[0]*wc[3]
+        wgs += wc[1]*wc[3]
+        wbs += wc[2]*wc[3]
+        sw += wc[3]
+    return((wrs/sw,wgs/sw,wbs/sw))
+
+def isPressed(): #Returns true once when the button is pressed
+    global pressed
+    if btn.value == False:
+        if pressed == False:
+            pressed = True
+            return True
+    else:
+        pressed = False
+        return False
 
 #Modes:
+
 class audioVisualizer():
-    def __init__(self, colors, rainbowness, blending, scrollSpeed):
-        global half
+    def __init__(self, colors, rainbowness, blending, scrollSpeed, colorDesc):
+        global HALF, BUFFER, modes
+        modes[0].insert(0,self)
         self.colors = colors
         self.scroll = 0.0 #Don't touch
         self.scrollSpeed = scrollSpeed #Scroll speed. Default is 1. Advisable to change inversely to rainbowness
@@ -77,17 +83,24 @@ class audioVisualizer():
         self.maxVal = 255 #Max R/G/B value. Setting this above 255 allows for inverted colors by wrapping around
         self.rainbowness = rainbowness #How many colors to display at once. Works if greater than 0, but the reasonable range is from 0 to 2. 80 is fun
         self.blending = blending #How blended the colors are. Default is 0.5. Works reasonably from 0 to 1
+        self.desc = "Music visualizer and/or scrolling lightshow"
+        self.colorDesc = colorDesc
         self.lightshow = []
-        for i in range(half):
+        for i in range(HALF):
             self.lightshow.append(0)
         pixels.fill((0,0,0))
+
+    def run(self):
+        if supervisor.runtime.serial_bytes_available:
+            self.update()
+        self.display()
 
     def update(self):
         value = bytes(input(), 'utf-8')
         temp1 = value.strip(b' ')
         temp2 = temp1.split(b' ')
         self.lightshow.clear()
-        for i in range(half):
+        for i in range(HALF):
             try:
                 val = temp2[min(i,len(temp2)-1)]
             except ValueError:
@@ -95,8 +108,7 @@ class audioVisualizer():
             self.lightshow.append(val)
 
     def display(self):
-        global half, buffer
-        for i in range(half):
+        for i in range(HALF):
             try:
                 val = min(100.0,float(self.lightshow[min(i,len(self.lightshow)-1)]))
             except ValueError:
@@ -106,34 +118,39 @@ class audioVisualizer():
             r = min(max(hue[0]*val*self.chroma+(math.pow(val,1.5)*self.whiteness),hue[0]*0.1),self.maxVal)
             g = min(max(hue[1]*val*self.chroma+(math.pow(val,1.5)*self.whiteness),hue[1]*0.1),self.maxVal)
             b = min(max(hue[2]*val*self.chroma+(math.pow(val,1.5)*self.whiteness),hue[2]*0.1),self.maxVal)
-            pixels[(half+i)+buffer] = (r,g,b)
-            pixels[(half-(i+1))+buffer] = (r,g,b)
+            pixels[(HALF+i)+BUFFER] = (r,g,b)
+            pixels[(HALF-(i+1))+BUFFER] = (r,g,b)
             i += 1
         self.scroll = self.scroll + ((0.1+self.scrollJuice) * self.scrollSpeed)
         self.scrollJuice = 0.0
         pixels.show()
 
 class fire():
-    def __init__(self, color):
-        global pixelCount, buffer
+    def __init__(self, color, colorDesc):
+        global N_PIXELS, BUFFER, modes
+        modes[1].insert(0,self)
         self.color = color
         self.smoothing = 0.01 #Flame smoothing
+        self.colorDesc = colorDesc
+        self.desc = "Fire animation"
         self.flamePixels = []
-        for i in range(pixelCount - buffer):
+        for i in range(N_PIXELS - BUFFER):
             self.flamePixels.append([0,0,0,0]) #hue, brightness, Flicker speed, Flicker direction
 
+    def run(self):
+        self.display()
+
     def display(self):
-        global pixelCount, buffer
         fp = self.flamePixels
-        for i in range(pixelCount - buffer):
+        for i in range(N_PIXELS - BUFFER):
             if fp[i][2] == 0: #Setup
                 fp[i][0] = random.randint(self.color - 5,self.color + 5) #Hue
                 fp[i][1] = random.randint(50,60) #Brightness
                 fp[i][2] = random.randint(5,20) #Flicker speed
                 fp[i][3] = 1 #Flicker direction
-            if 0 < i < pixelCount - 1: #Smoothing
+            if 0 < i < N_PIXELS - 1: #Smoothing
                 fp[i][1] -= (fp[i][1]-fp[max(i-1,0)][1]) * self.smoothing
-                fp[i][1] -= (fp[i][1]-fp[min(i+1,pixelCount-buffer-1)][1]) * self.smoothing
+                fp[i][1] -= (fp[i][1]-fp[min(i+1,N_PIXELS-BUFFER-1)][1]) * self.smoothing
             if fp[i][3] == 1: #Flicker up
                 fp[i][1] += fp[i][2] / 1
                 fp[i][0] -= fp[i][2] / 25
@@ -150,47 +167,84 @@ class fire():
                     fp[i][0] = random.randint(self.color - 5,self.color + 5)
             hue = wheel(fp[i][0],0.5) #Display
             br = fp[i][1] / 255
-            pixels[i + buffer] = (hue[0]*br,hue[1]*br,0)
+            pixels[i + BUFFER] = (hue[0]*br,hue[1]*br,0)
         pixels.show()
 
-print("Running...") #Startup
-av1 = audioVisualizer([(255,0,0),(0,255,0),(0,0,255)],1, 0.7, 3)
-av2 = audioVisualizer([(10,230,10),(255,255,255),(255,10,0)],0.5, 0.1, 5)
-fr1 = fire(20)
+#Main
 
-while True: #Running loop
-    dcWait = dcWait + 1
-    if btn.value == False:
-        if pressed == False:
-            pressed = True
-            if dcWait < 20: #Double click to change mode
-                animationMode = animationMode + 1
-                if animationMode > 2:
-                    animationMode = 0
-                print("Mode is now mode: " + str(animationMode))
-                brightness += brIncrement
-                pixels.brightness = brightness
-                dcWait = 20
-            else:
-                brightness -= brIncrement
-                if brightness < -0.01 or brightness > 1: #floating point error solution
-                    brightness = 1
-                pixels.brightness = brightness
-                print("Brightness is now: " + str(brightness * 100) + "%")
+def setup():
+    #Order is reversed from the order they are established in
+    av1 = audioVisualizer([(255,0,0),(0,255,0),(0,0,255)],1, 0.7, 2, "Rainbow") #colors, rainbowness, blending, scrollSpeed, color description
+    av2 = audioVisualizer([(10,230,10),(255,255,255),(255,10,0)],0.5, 0.1, 5, "Mexico")
+    fr1 = fire(20, "Orange Red") #Hue, color description
+
+def main():
+    global btn, modes
+    btn.direction = Direction.INPUT
+    btn.pull = Pull.UP
+    pressed = False #Button value
+    longPressTest = False
+    longPressTimer = 0
+    dcTest = False #Prevents single clicking while double clicking
+    dcWait = 0 #Double click timer
+
+    animationMode = 0
+    colorMode = 0
+
+    brightness = 1 #LED brightness
+    BR_INC = 0.2 #Amount the brightness changes per button press. Works from 0 to 1. Default is 0.2
+
+    while True:#Running loop
+
+        if not btn.value and longPressTest:
+            longPressTimer += 1
+
+        if dcTest:
+            dcWait += 1
+
+        if isPressed(): #Fires on button press
+            if not longPressTest:
+                longPressTest = True
+                longPressTimer = 0
+            if not dcTest:
+                dcTest = True
                 dcWait = 0
+            else:
+                if dcWait <= 10: #Double press condition
+                    if animationMode == len(modes) - 1: #List buffer isn't counted by len
+                        animationMode = 0
+                    else:
+                        animationMode += 1
+                    colorMode = 0
+                    print("Mode is: " + modes[animationMode][0].desc)
+                    print("Color is: " + modes[animationMode][colorMode].colorDesc)
+                    dcTest = False
+                    longPressTest = False
 
-    else:
-        pressed = False
+        if btn.value and longPressTest and longPressTimer >= 5: #Long press condition
+            if colorMode == len(modes[animationMode]) - 3: #List buffer is counted by len for some reason
+                colorMode = 0
+            else:
+                colorMode += 1
+            print("Color is: " + modes[animationMode][colorMode].colorDesc)
+            longPressTimer = 0
+            longPressTest = False
+            dcTest = False
 
-    if (animationMode == 0):
-        if supervisor.runtime.serial_bytes_available:
-            av1.update()
-        av1.display()
+        if btn.value and dcTest and dcWait > 10: #Short press condition
+            brightness -= BR_INC
+            if brightness < -0.01 or brightness > 1: #Deals with floating point errors
+                brightness = 1
+            pixels.brightness = brightness
+            if brightness > 0.001: #Also deals with floating point errors
+                print("Brightness is: " + str(brightness * 100) + "%")
+            else:
+                print("Brightness is: 0%")
+            longPressTimer = 0
+            longPressTest = False
+            dcTest = False
 
-    if (animationMode == 2):
-        if supervisor.runtime.serial_bytes_available:
-            av2.update()
-        av2.display()
+        modes[animationMode][colorMode].run()
 
-    if (animationMode == 1):
-        fr1.display()
+setup()
+main()
